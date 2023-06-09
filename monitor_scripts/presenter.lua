0469338d9360bfc8549fb107e98f0ee8d445617c
glassinatorn <glass1995@gmail.com>
2023-06-09 16:35:27 +0200

    fixing local import

--- monitor_scripts/presenter.lua
+++ monitor_scripts/presenter.lua
@@ -4,7 +4,7 @@
 local original_path = package.path -- saving original path
 package.path = original_path .. ";..help_functions/?.lua" -- adding path to help functions
 
-local debugging = require(".help_functions.debugging") -- importing local debugging functions
+local debugging = require("debugging")
 
 package.path = original_path -- resetting original path
 
