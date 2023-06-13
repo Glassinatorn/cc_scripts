b72663e09e9aa836beb564ee19f1106c67a61d34
glassinatorn <glass1995@gmail.com>
2023-06-13 10:37:57 +0200

    implementing new http request

--- help_functions/chatgpt.lua
+++ help_functions/chatgpt.lua
@@ -1,24 +1,42 @@
 function gatherResponse(prompt, api_key)
-  -- Encode the prompt for HTTP request
-  local encodedPrompt = http.encodeURIComponent(prompt)
+    -- prepare the request
+    local request = [[{ "prompt": "]] .. prompt .. [[", "max_tokens": 150, }]]
+    local url = "https://api.openai.com/v1/engines/davinci-codex/completions"
 
-  -- Make a GET request to the OpenAI API
-  local url = "https://api.openai.com/v1/engines/davinci-codex/completions?prompt=" .. encodedPrompt
-  local headers = {
-    ["Content-Type"] = "application/json",
-    ["Authorization"] = "Bearer " .. api_key -- Replace with your OpenAI API key
-  }
+    local headers = {
+        ["Content-Type"] = "application/json",
+        ["Authorization"] = "Bearer " .. api_key
+    }
 
-  local response = http.get(url, headers)
-  local responseBody = response.readAll()
-  response.close()
+    local response = http.post(url, headers, request)
+    local responseBody = response.readAll()
+    response.close()
 
-  -- Parse the response body and extract the generated text
-  local parsedResponse = http.jsonParse(responseBody)
-  local generatedText = parsedResponse.choices[1].text
+    local parsedResponse = http.jsonParse(responseBody)
+    local generatedText = parsedResponse.choices[1].text
 
-  -- Return the generated text
-  return generatedText
+    return generatedText
+
+  -- -- Encode the prompt for HTTP request
+  -- local encodedPrompt = http.encodeURIComponent(prompt)
+
+  -- -- Make a GET request to the OpenAI API
+  -- local url = "https://api.openai.com/v1/engines/davinci-codex/completions?prompt=" .. encodedPrompt
+  -- local headers = {
+  --    ["Content-Type"] = "application/json",
+  --    ["Authorization"] = "Bearer " .. api_key -- Replace with your OpenAI API key
+  -- }
+
+  -- local response = http.get(url, headers)
+  -- local responseBody = response.readAll()
+  -- response.close()
+
+  -- -- Parse the response body and extract the generated text
+  -- local parsedResponse = http.jsonParse(responseBody)
+  -- local generatedText = parsedResponse.choices[1].text
+
+  -- -- Return the generated text
+  -- return generatedText
 end
 
 return { gatherResponse = gatherResponse }
