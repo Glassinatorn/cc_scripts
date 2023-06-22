ddb96facf49131cbdabc666e4aae5de30e7036ea
glassinatorn <glass1995@gmail.com>
2023-06-23 00:28:13 +0200

    trying new connection to chatgpt

--- help_functions/chatgpt.lua
+++ help_functions/chatgpt.lua
@@ -8,37 +8,61 @@ end
     
 function gatherResponse(prompt, api_key)
     -- prepare the request
-    local url = "https://api.openai.com/v1/completions"
-    local request = '{' ..
-        '"model": "text-davinci-003",' ..
-        '"prompt" : "' .. prompt .. '",' ..
-        '"temperature": 0.9,' ..
-        '"max_tokens": 150,' ..
-        '"top_p": 1,' ..
-        '"frequency_penalty": 0.0,' ..
-        '"presence_penalty": 0.6,' ..
-        '"stop": [" Human:", " AI:"]' ..
-    '}'
-
+    local url = "https://api.openai.com/v1/chat/completions"
+    local max_tokens = 150
+    local model = "gpt3-3.5-turbo"
     local headers = {
         ["Content-Type"] = "application/json",
         ["Authorization"] = "Bearer " .. api_key
     }
+    local request = {
+        ["model"] = model,
+        ["max_tokens"] = max_tokens, 
+        ["messages"] = {
+            role = "user",
+            content = prompt
+        }
+    }
+    local payload_enc = textutils.serializeJSON(request)
 
-    local response = http.post(url, request, headers)
+    local requesting = true
+    http.request(url, payload_enc, headers)
 
-    if response == nil then
-        error("Failed to make request to OpenAI API")
+    while requesting do
+        local event, url, sourceText = os.pullEvent()
 
-        return nil
-    else
-        local responseBody = response.readAll()
-        response.close()
+        if event == "http_success" then
+            local response = sourceText.readAll()
+            sourceText.close()
+            local response_json = textutils.unserializeJSON(response)
 
-        local parsedResponse = http.jsonParse(responseBody)
-        local generatedText = parsedResponse.choices[1].text
+            if response_json and response_json.choices and response_json.choices[1] then
+                local generatedText = response_json.choices[1].text
+                return generatedText
+            else
+                error("Failed to make request to OpenAI API")
+            end
+            requesting = false
 
-        return generatedText
+        elseif event == "http_failure" then
+            print("Failed to make request to OpenAI API")
+            requesting = false
+        end
+
+    --local response = http.post(url, request, headers)
+
+    --if response == nil then
+        --error("Failed to make request to OpenAI API")
+
+        --return nil
+    --else
+        --local responseBody = response.readAll()
+        --response.close()
+
+        --local parsedResponse = http.jsonParse(responseBody)
+        --local generatedText = parsedResponse.choices[1].text
+
+        --return generatedText
     end
 end
 
