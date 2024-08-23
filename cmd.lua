d9324535e28a887d35fca007be05e2bc9197c4d9
Glassinatorn <gusten1995@gmail.com>
2024-08-23 03:24:37 +0200

    Added a main loop

--- cmd.lua
+++ cmd.lua
@@ -2,3 +2,14 @@ local move = require("build.move")
 local build = require("build.build_stuff")
 
 print("test")
+
+function main()
+    write("What do you want to do?")
+    write("1: build a bridge")
+    local msg = read()
+
+
+    if msg == "1" then
+        build.bridge()
+    end
+end
